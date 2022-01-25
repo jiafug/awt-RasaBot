d67c09fc14d73f1d7bce45c5f08a6ff28ab2c99a
jiafug <comet.liu@protonmail.com>
2022-01-25 17:22:52 +0100

    nlg server: integration of domain file

--- nlg/server.py
+++ nlg/server.py
@@ -1,6 +1,12 @@
 import json
+import os
+import random
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+import yaml
+
+script_dir = os.path.dirname(__file__)  # <--- absolute dir the script is in
+domain = os.path.join(script_dir, "../domain.yml")  # <--- rasa domain file
 hostName = "localhost"
 serverPort = 5054
 
@@ -25,12 +31,21 @@ class NLGServer(BaseHTTPRequestHandler):
     def do_POST(self):
         if self.path == '/nlg':
             self._set_response()
-            content_length = int(
-                self.headers['Content-Length'])  # <--- Gets the size of data
-            post_data = self.rfile.read(content_length).decode(
-                "utf-8")  # <--- Gets the data itself
-            topic = NLGServer.parse_rasa_request(post_data)
-            response = NLGServer.get_rasa_response(topic)
+            # length of request
+            content_length = int(self.headers['Content-Length'])
+            # body of request
+            post_data = self.rfile.read(content_length).decode("utf-8")
+            # get current topic
+            topic, action = NLGServer.parse_rasa_request(post_data)
+            if "qa" in action:
+                # q&a response from document store
+                pass
+            else:
+                # get static response
+                response_txt = NLGServer.get_static_bot_response(action)
+            # create rasa response
+            response = NLGServer.create_rasa_response(response_txt)
+            # write response
             self.wfile.write(bytes(json.dumps(response), 'utf-8'))
         else:
             self.send_error(404, "not found",
@@ -39,15 +54,24 @@ class NLGServer(BaseHTTPRequestHandler):
     @staticmethod
     def parse_rasa_request(data):
         request = json.loads(data)
-        response = request['response']
+        action = request['response']
         events = request['tracker']['events']
-        print(events)
-        return ""
+        return "", action
 
     @staticmethod
-    def get_rasa_response(topic):
+    def get_static_bot_response(action):
+        with open(domain, 'r') as stream:
+            data_loaded = yaml.safe_load(stream)
+            # get a list of all responses for a specific action
+            text_list = data_loaded['responses'][action]
+            # pick a random respone
+            rand_res = random.choice(text_list)
+            return rand_res['text']
+
+    @staticmethod
+    def create_rasa_response(response_txt):
         json_response = {
-            "text": "Some text",
+            "text": response_txt,
             "buttons": [],
             "image": None,
             "elements": [],
@@ -60,11 +84,9 @@ class NLGServer(BaseHTTPRequestHandler):
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), NLGServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
-
     try:
         webServer.serve_forever()
     except KeyboardInterrupt:
         pass
-
     webServer.server_close()
     print("Server stopped.")
